95f95f9f4301e8615a262f1cc9addac452bcb43e
lucasg52 <lucasg52@github.com>
2024-05-31 12:00:41 -0500

    fixed spelling of propagation

--- solver-groundwork/searchclasses.py
+++ solver-groundwork/searchclasses.py
@@ -9,18 +9,18 @@ class GNASearch():
     def __init__(
                  self,
                  lo, hi,                # the assumed window in which the solution lies
-                 propotest,             # the function that will return True if propogation succeeds, False otherwise
+                 propatest,             # the function that will return True if propagation succeeds, False otherwise
                  a = math.nan           # optionally, a first guess can be provided (otherwise is the median between lo and hi)
                 ):
         self.lo = lo
         self.hi = hi
         self.term = False
-        self.propotest = propotest
+        self.propatest = propatest
         if math.isnan(a):
             self.a = (lo + hi) / 2
 
     def searchstep (self):
-        if self.propotest(self.a):
+        if self.propatest(self.a):
             self.hi = self.a
         else:
             self.lo = self.a
@@ -32,22 +32,22 @@ class ExpandingSearch(GNASearch):
     def __init__(
                  self,
                  lo, hi,                # the assumed window in which the solution lies
-                 propotest,             # the function that will return True if propogation succeeds, False otherwise
+                 propatest,             # the function that will return True if propagation succeeds, False otherwise
                  lim_lo, lim_hi,        # since the search may expand, these will provide hard upper and lower bounds
                                         # (NOT IMPLIMENTED YET)
                  a = math.nan           # optionally, a first guess can be provided (otherwise is the median between lo and hi)
                 ):
         self.lim_lo = lim_lo
         self.lim_hi = lim_hi
-        super().__init__(lo, hi, propotest, a = a)
+        super().__init__(lo, hi, propatest, a = a)
 
     def searchstep(self):
         d = self.hi - self.lo
-        if not self.propotest(self.hi):
+        if not self.propatest(self.hi):
             self.hi += 2 * d
             self.lo += d
             self.a = (self.lo + self.hi) / 2
-        elif self.propotest(self.lo):
+        elif self.propatest(self.lo):
             self.lo -= 2 * d
             self.hi -= d 
             self.a = (self.lo + self.hi) / 2
