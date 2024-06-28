341ab9219b6d337ff058cd6b88a445aa9399d779
zittlowl <zittlowl@ripon.edu>
2024-06-28 09:44:21 -0500

    Update battling_oscillations.py

--- experimentation/battling_oscillations.py
+++ experimentation/battling_oscillations.py
@@ -50,20 +50,20 @@ h.finitialize(-65)
 h.continuerun(20)
 
 #spikes
-print(f"number of spikes: {len(list(st_prop))}")
-print(f"spike times prop 0: {list(st_prop)}")
+print(f"number of spikes at prop 0: {len(list(st_prop))}")
+#print(f"spike times prop 0: {list(st_prop)}")
 
-print(f"number of spikes: {len(list(st_prop2))}")
-print(f"spike times prop 0.0625: {list(st_prop2)}")
+print(f"number of spikes at porp 0.0625: {len(list(st_prop2))}")
+#print(f"spike times prop 0.0625: {list(st_prop2)}")
 
-print(f"number of spikes: {len(list(st_main))}")
-print(f"spike times main 1: {list(st_main)}")
+print(f"number of spikes at main 1: {len(list(st_main))}")
+#print(f"spike times main 1: {list(st_main)}")
 
-print(f"number of spikes: {len(list(st_IS))}")
-print(f"spike times IS 0.5: {list(st_IS)}")
+print(f"number of spikes at IS 0.5: {len(list(st_IS))}")
+#print(f"spike times IS 0.5: {list(st_IS)}")
 
-print(f"number of spikes: {len(list(st_soma))}")
-print(f"spike times soma 0: {list(st_soma)}")
+print(f"number of spikes at soma 0: {len(list(st_soma))}")
+#print(f"spike times soma 0: {list(st_soma)}")
 
 #plotting
 plt.plot(t,prop_v0,label="prop 0")
