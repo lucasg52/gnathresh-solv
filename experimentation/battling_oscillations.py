971950a6efbdaae05dc925dc1692ff530036493f
zittlowl <zittlowl@ripon.edu>
2024-06-28 10:12:35 -0500

    Update battling_oscillations.py

--- experimentation/battling_oscillations.py
+++ experimentation/battling_oscillations.py
@@ -57,27 +57,27 @@ def run():
   h.continuerun(20)
 
 #spikes
-def spikes():
-  print(f"number of spikes at prop 0: {len(list(st_prop))}")
-  print(f"spike times prop 0: {list(st_prop)}")
-
-  print(f"number of spikes at porp 0.0625: {len(list(st_prop2))}")
-  print(f"spike times prop 0.0625: {list(st_prop2)}")
-
-  print(f"number of spikes at main 1: {len(list(st_main))}")
-  print(f"spike times main 1: {list(st_main)}")
-  
-  print(f"number of spikes at IS 0.5: {len(list(st_IS))}")
-  print(f"spike times IS 0.5: {list(st_IS)}")
-
-  print(f"number of spikes at IS 1: {len(list(st_IS2))}")
-  print(f"spike times IS 1: {list(st_IS2)}")
-
-  print(f"number of spikes at soma 0: {len(list(st_soma))}")
-  print(f"spike times soma 0: {list(st_soma)}")
+def spikes(number, place):
+  if number=='yes':
+    print(f"number of spikes at prop 0: {len(list(st_prop))}")
+    print(f"number of spikes at porp 0.0625: {len(list(st_prop2))}")
+    print(f"number of spikes at main 1: {len(list(st_main))}")
+    print(f"number of spikes at IS 0.5: {len(list(st_IS))}")
+    print(f"number of spikes at IS 1: {len(list(st_IS2))}")
+    print(f"number of spikes at soma 0: {len(list(st_soma))}")
+    
+  if place=='yes':
+    print(f"spike times prop 0: {list(st_prop)}")
+    print(f"spike times prop 0.0625: {list(st_prop2)}")
+    print(f"spike times main 1: {list(st_main)}")  
+    print(f"spike times IS 0.5: {list(st_IS)}")  
+    print(f"spike times IS 1: {list(st_IS2)}")
+    print(f"spike times soma 0: {list(st_soma)}")
+  else:
+    print("number or place?")
 
 #plotting
-def plotting():
+def plot():
     plt.plot(t,prop_v0,label="prop 0")#, linestyle='dashed')
     plt.plot(t, prop_v00625, label = "prop 0.0625")#, linestyle='dashed')
     plt.plot(t,main_v1,label="main 1")#, linestyle='dashed')
