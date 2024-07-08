3df0509b0ad83026091b47c659e7bb1944767650
zittlowl <zittlowl@ripon.edu>
2024-07-08 11:39:40 -0500

    for organization purposes

--- experimentation/testing_Rin.py
+++
@@ -1,8 +0,0 @@
-from gnatsolv.cells import base
-from neuron import h
-from matplotlib import pyplot as plt
-
-
-class resist_cell(BaseExpCell(0.2,3)):
-	def collat():
-		self.parent = h.Section()
--- expermt/jul01.py
+++ expermt/jul01.py
@@ -1,7 +1,7 @@
 from neuron import h
 #from gnatsolv.cells.tools import APRecorder
-from gnatsolv.cells.tapertypes import ExpCell_notaper
-from gnatsolv.solver import searchclasses
+from ..cells.tapertypes import ExpCell_notaper
+from ..solver import searchclasses
 import matplotlib.pyplot as plt
 h.load_file("stdrun.hoc")
 m = ExpCell_notaper(0.2,3)
