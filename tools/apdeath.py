0771eaee907e072529d4eda4dcf6e5a77a25e38b
lucasg52 <lucasg52@github.com>
2024-07-09 17:13:19 -0500

    started work on Deathrecorder

--- tools/apdeath.py
+++ tools/apdeath.py
@@ -1,14 +1,32 @@
 from .aprecorder import APRecorder
 from neuron import h
-class DeathTester:
-    def __init__(self, tree, timeout, tinterval = 1):
+class DeathTester(APRecorder):
+    def __init__(self, tree, timeout, tinterval = 1, threshold = -50, maxsteps = 100):
         self.tree = tree
         self.timeout = timeout
         self.tinterval = tinterval
+        self.threshold = threshold
         self._initnodes()
     def _initnodes(self):
         self.nodes = []
         for sec in self.tree:
             if sec.parentseg().sec in self.tree:
                 self.nodes.append(sec(1))
-    
+    def proptest(self):
+        for i in range(self.maxsteps):
+            superret = super().proptest()
+            if not superret:
+                if self.lifetest():
+                    h.continuerun(self.interval) # we are still alive
+                else:       # not propogating and we dead a hell
+                    break
+            else:
+                self.proptime = self.recorded[0]
+                return superret
+        return 0
+    def lifetest(self):
+        """returns true if there are any nodes where the AP is still alive"""
+        for n in self.nodes:
+            if n.v > self.threshold:
+                return True
+        return False
--- tools/aprecorder.py
+++ tools/aprecorder.py
@@ -6,15 +6,11 @@ class APRecorder():
             sec,
             ran = 0.5
             ):
-        self.recorded = False
+        self.recorded = h.Vector()
         self.nc = h.NetCon(sec(ran)._ref_v, None, sec = sec)
-        self.nc.record(self._record)
-    def _record(self):
-        """called by hoc during simulation when an AP is recorded"""
-        #print(str(self) + "recorded an AP")
-        self.recorded = True
+        self.nc.record(self.recorded)
+
     def proptest(self):
-        """read self.recorded and reset it to False"""
-        ret = self.recorded
-        self.recorded = False
+        """returns len(self.recorded)"""
+        ret = len(self.recorded)
         return ret
