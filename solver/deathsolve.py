d1ad99f95d0cfef1ddacd88e84bbe06ce862506d
lucasg52 <lucasg52@github.com>
2024-07-19 10:57:45 -0500

    added some experimental functionality to deathsolve

--- solver/deathsolve.py
+++ solver/deathsolve.py
@@ -1,4 +1,4 @@
-from .newton import DiscreteNewton as DNewton
+from .newton import dualderiv_inv ,DiscreteNewton_dualfun as DNewton
 #from ..tools.apdeath import DeathTester
 from ..tools.aprecorder import APRecorder
 from neuron import h
@@ -32,7 +32,7 @@ class SegDeathSolver:
 			vinitial,
 			interval = 3,
 			maxsteps = 100,
-            coef = pow(2, -2)
+            coef = pow(2, -4)
             ):
         self.lo = lo
         self.hi = hi
@@ -43,11 +43,11 @@ class SegDeathSolver:
         self.interval = interval
         self.maxsteps = maxsteps
         self.coef = coef
-        self.search = DNewton(self.timetest, a, (self.hi - self.lo) * coef)
+        self.search = DNewton(self.timedualderiv, a, (self.hi - self.lo) * coef)
         self.rec = h.DeathRec(self.seg)
         self.aprec = APRecorder(self.seg.sec, self.seg.x)
         self.aprec.nc.threshold = -10
-    def _initnodes(self):
+    def _initnodes(self):       #should be changed to have more nodes
         self.nodes = [h.DeathRec(self.seg)]
 
     def timetest(self, g):
@@ -66,6 +66,17 @@ class SegDeathSolver:
                     break
         return self.rec.deatht 
 
+    def timedualderiv(self,g , dg, adjustdx = True):
+        dx = self.search.dx
+        fa = self.timetest(g)
+        fb = self.timetest(g+dx)
+        fc = self.timetest(g+ 2*dx)
+        if adjustdx:
+            minsteps = int(min(fb-fa, fc-fb) / h.dt)
+            if minsteps > 4:
+                self.search.dx /= minsteps-2
+                print("new dx" + str(self.search.dx))
+        return dx/(fb-fa), dx/(fc-fb)
 
     def solve(self, acc):
         if (self.hi - self.lo) < acc:
@@ -73,11 +84,12 @@ class SegDeathSolver:
         while (self.hi - self.lo) > acc:
             line = self.search.searchstep()
             print(line)
-            if self.aprec.proptest():
-                self.hi = line.inter
-            else:
-                self.lo = line.inter
-            self.search.dx = (self.hi - self.lo) * self.coef
+            #if self.aprec.proptest():
+            #    self.hi = line.inter
+            #else:
+            #    self.lo = line.inter
+            #assert self.lo < self.hi
+            #self.search.dx = (self.hi - self.lo) * self.coef
 
     def _set_tstop(self, val):
         h.tstop = self.__tstop = val
--- solver/newton.py
+++ solver/newton.py
@@ -1,6 +1,17 @@
 import math
 
 
+def dualderiv(a, dx, fun):
+    fa = fun(a)
+    fb = fun(a+dx)
+    fc = fun(a+2 * dx)
+    return (fb-fa)/dx, (fc-fb)/dx
+def dualderiv_inv(a, dx, fun):
+    fa = fun(a)
+    fb = fun(a+dx)
+    fc = fun(a+2 * dx)
+    return dx/(fb-fa), dx/(fc-fb)
+
 class Line():
     def __init__(self, a, b):
         self.a = a
@@ -38,6 +49,20 @@ class DiscreteNewton:
         p_a = (a, self.fun(a))
         p_b = (b, self.fun(b))
         line = Line(p_a, p_b)
+        self.a = line.inter
+        return line
+
+class DiscreteNewton_dualfun(DiscreteNewton):
+    def searchstep(self, dx = math.nan):
+        a = self.a
+        if math.isnan(dx):
+            dx = self.dx
+        assert(abs(dx) > 2* math.ulp(a))
+        f_a, f_b = self.fun(a,dx)
+        line = Line(
+                (a, f_a),
+                ((a + dx), f_b)
+                )
         self.a = line.inter
         return line
 
