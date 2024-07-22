a1ff7d551d2da3b76b37d11f2cbb647c5680b424
zittlowl <zittlowl@ripon.edu>
2024-07-22 08:09:48 -0500

    Work for figuring out how to get them to have equivalent Rins

--- expermt/Laura/testing_new_features.py
+++ expermt/Laura/testing_new_features.py
@@ -21,22 +21,28 @@ def adjust_factor(len,dl):
 	n._normalize()
 	n.side1.disconnect()
 
-def adjust_addent(len,dl):
-	n.side1.L = (len-dl)*elength(n.side1)
-	n.dau1.L = (len-dl)*elength(n.dau1)
-	n.dau2.L = (len-dl)*elength(n.dau2)
-	n.side1.connect(n.main_shaft(0.6))
-	n._normalize()
-	n.side1.disconnect()
+# def adjust_addent(len,dl):
+# 	n.side1.L = (len-dl)*elength(n.side1)
+# 	n.dau1.L = (len-dl)*elength(n.dau1)
+# 	n.dau2.L = (len-dl)*elength(n.dau2)
+# 	n.side1.connect(n.main_shaft(0.6))
+# 	n._normalize()
+# 	n.side1.disconnect()
 
 def adjust(len):
-	n.side1.L = (len/(len/0.061619162))*elength(n.side1)
-	n.dau1.L = (len/(len/0.061619162))*elength(n.dau1)
-	n.dau2.L = (len/(len/0.061619162))*elength(n.dau2)
+	n.side1.L = (0.0285736*len)*elength(n.side1)
+	n.dau1.L = (0.0285736*len)*elength(n.dau1)
+	n.dau2.L = (0.0285736*len)*elength(n.dau2)
 	n.side1.connect(n.main_shaft(0.6))
 	n._normalize()
 	n.side1.disconnect()
 	print(n.side1.L/118.84, n.dau1.L/118.84, n.dau2.L/118.84)
+
+def m_adjust(len):
+	m.side1.L = len*elength(m.side1)
+	m.side1.connect(m.main_shaft(0.6))
+	m._normalize()
+	m.side1.disconnect()
 
 def test():
 	adj_lst = [j/1000 for j in range(800, 35000, 1)]
@@ -46,26 +52,33 @@ def test():
 	len_lst=[]
 	fin_results = []
 	min_diff = []
+	act_len = []
 	for k in lengths:
 		print(f"for length {k}")
+		m_adjust(k)
 		# adjust(k)
 		# M = m.Rin(m.side1(0))
 		# N = n.Rin(n.side1(0))
-		# adj_lst2.append(k/(k/0.061619162))
+		# adj_lst2.append(0.0285736*k)
 		# diff_lst.append(M-N)
 		# fin_results.append(k)
+
 		# if k < 0.5:
+
 		for i in adj_lst:
 			adjust_factor(k, i)
 			M = m.Rin(m.side1(0))
 			N = n.Rin(n.side1(0))
 			min_diff.append(abs(M-N))
-		print(f"{adj_lst[min_diff.index(min(min_diff))]} with a diff of {min(min_diff)} and the lengths are {n.side1.L/118.84, n.dau1.L/118.84, n.dau2.L/118.84}")
+			len_lst.append(n.side1.L / 118.84)
+		print(f"{adj_lst[min_diff.index(min(min_diff))]} with a diff of {min(min_diff)} and the lengths are {n.side1.L/118.84, n.dau1.L/118.84, n.dau2.L/118.84, m.side1.L/118.84}")
 		adj_lst2.append(adj_lst[min_diff.index(min(min_diff))])
+		act_len.append(len_lst[min_diff.index(min(min_diff))])
 		diff_lst.append(min(min_diff))
 		fin_results.append(k)
-		len_lst.append(n.side1.L/118.84)
 		min_diff=[]
+		len_lst = []
+
 		# else:
 		# 	for l in range(0,int(k*100), 1):
 		# 		# print(k-(l/100))
@@ -78,7 +91,7 @@ def test():
 		# 			adj_lst2.append(l/100)
 		# 			diff_lst.append(M-N)
 		# 			fin_results.append(k)
-	return adj_lst2, diff_lst, fin_results, len_lst
+	return adj_lst2, diff_lst, fin_results, act_len
 
 def plot():
 	fig, ax = plt.subplots(nrows=2,ncols=1,figsize=(8,4))
@@ -95,7 +108,7 @@ def plot():
 	# ax.set_zlabel('difference')#'input resistance')
 	# ax.set_title('creating equal input resistances')
 	# plt.show()
-	ax[0].plot(fin_results, len_lst, label="length")
+	ax[0].plot(len_lst, diff_lst, label="length")
 	ax[1].plot(fin_results, adj_lst2, label="factor")
 	ax[0].grid()
 	ax[1].grid()
