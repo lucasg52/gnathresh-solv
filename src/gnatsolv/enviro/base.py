4892bd9f955a1934e649eb06a5f0688c8a6ac02e
Lucas Swanson <141633910+lucasg52@users.noreply.github.com>
2024-10-01 11:49:48 -0500

    Update base.py

--- src/gnatsolv/enviro/base.py
+++ src/gnatsolv/enviro/base.py
@@ -1,12 +1,12 @@
 """
 Environments will be the standard method of setting up experiments that involve calculating gna thresh for a particular geometry or family of cell geometries.
 Instance attributes:
-m:         m(y Cell) -- the cell for which gna thresh is computed
-stim:      The stimulus for the cell. Often an h.IClamp
+    m:         m(y Cell) -- the cell for which gna thresh is computed
+    stim:      The stimulus for the cell. Often an h.IClamp
 
 Instance methods:
-fullsolve:   Compute the gna thresh for the cell, given some arguments for optimization.
-prerun(gna): Achieve a steady state by running a large time step from the initial condition, from a negative value for t.
+    fullsolve:   Compute the gna thresh for the cell, given some arguments for optimization.
+    prerun(gna): Achieve a steady state by running a large time step from the initial condition, from a negative value for t.
 """
 
 from abc import ABC, abstractmethod
