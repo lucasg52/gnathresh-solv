31eae24469066f521279852ff8d07482f28794d8
lucasg52 <lucasg52@github.com>
2024-10-05 14:08:20 -0500

    added a tester

--- src/gnatsolv/tools/apdeath.py
+++ src/gnatsolv/tools/apdeath.py
@@ -39,14 +39,6 @@ class DeathRec:
         self.deathvec = h.Vector()
         self.beginvec = h.Vector()
         #else:
-        #    print (self.nodes)
-        #    h.RangeVarPlot(
-        #            lambda segx : self.nodes.append(h.cas()(segx)),
-        #            beginseg, endseg
-        #            )
-        #    print (self.nodes)
-        #    for n in self.nodes: 
-        #        n.insert("apdeath")
 
     def run(self):
         h.continuerun(self.tstop)
@@ -59,21 +51,12 @@ class DeathRec:
             h.continuerun(h.t + self.tinterval)
         if i == 0:
             print(f"DeathRec: WARNING: did not detect cell stimulation between t = {self.tstop} and t = {h.t}")
-            print(self.lifetest())
+            print(f"DeathRec: lifetest: {self.lifetest()}")
     def prelifetest(self):
-        #return all(
-        #            node.begin_apdeath == 0 
-        #            for node in self.nodes
-        #        )
-        #return h.flagbegin_apdeath == 0
         self.beginrvp.to_vector(self.beginvec)
         return self.beginvec.max() == 0
 
     def lifetest(self):
-        #return not all(
-        #        bool(node.begin_apdeath) == bool(node.deatht_apdeath)
-        #        for node in self.nodes
-        #        )
         self.beginrvp.to_vector(self.beginvec)
         self.deathrvp.to_vector(self.deathvec)
         return (self.beginvec.sub(self.deathvec)).max() > 0
