adb46e6584486057382bc130a27c64e20b4193df
Lucas Swanson <141633910+lucasg52@users.noreply.github.com>
2024-10-15 18:19:37 -0500

    Update apdeath.py

--- src/gnatsolv/tools/apdeath.py
+++ src/gnatsolv/tools/apdeath.py
@@ -2,13 +2,12 @@ import time
 from neuron import h
 from .aprecorder import APRecorder
 from .oldapdeath import DeathTester
-#from .. import eq
+
 class DeathRec:
     def __init__(
             self,
-            recbegin, recend,
-            tstop,                  # ms
-            #recinterval = 0.25,     # lambda
+            recbegin, recend,       # for the time being, these must be the same h.Section. (the recodring zone will only be that section)
+            tstop,                  # NOTE: should be refactored to tstart
             minapspeed = 1,         # lambda/ms (unused)
             tinterval  = 0.25,      # ms
             maxsteps   = 100        
@@ -19,12 +18,9 @@ class DeathRec:
         self.aprec = APRecorder(recend, 1)
         self.proptest = self.aprec.proptest
         self.tstop = tstop
-        #self.recinterval = recinterval
         self.minapspeed =  minapspeed 
         self.tinterval  =  tinterval 
         self.maxsteps = maxsteps
-
-
 
     def _setup_nodes(self, begin, end):
         beginseg = begin(0)
@@ -38,9 +34,12 @@ class DeathRec:
         self.beginrvp = h.RangeVarPlot("begin_apdeath", beginseg, endseg)
         self.deathvec = h.Vector()
         self.beginvec = h.Vector()
-        #else:
 
     def run(self):
+        """
+        Run the simulation until membrane activity in the recording zone has died
+        Prints warnings if no stimulation is detected after t = self.maxsteps * self.tinterval
+        """
         h.continuerun(self.tstop)
         i = self.maxsteps
         while i and self.prelifetest():
@@ -51,7 +50,8 @@ class DeathRec:
             h.continuerun(h.t + self.tinterval)
         if i == 0:
             print(f"DeathRec: WARNING: did not detect cell stimulation between t = {self.tstop} and t = {h.t}")
-            print(f"DeathRec: lifetest: {self.lifetest()}")
+            # print(f"DeathRec: lifetest: {self.lifetest()}")
+            
     def prelifetest(self):
         self.beginrvp.to_vector(self.beginvec)
         return self.beginvec.max() == 0
@@ -60,7 +60,13 @@ class DeathRec:
         self.beginrvp.to_vector(self.beginvec)
         self.deathrvp.to_vector(self.deathvec)
         return (self.beginvec.sub(self.deathvec)).max() > 0
+        
     def getdeathtime(self):
+        """
+        Calculates and returns the time at which membrane activity in recording zone has dropped below DEATHMECH_VTHRESH
+        (see deathmech.mod for details)
+        Sets self.deathtime to the calculated value
+        """
         self.deathrvp.to_vector(self.deathvec)
         self.deathtime = self.deathvec.max() 
         return self.deathtime
