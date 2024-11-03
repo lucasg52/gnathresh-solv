5fefbcfd57d5eedccacae17deb9cb85e21fe7dab
zittlowl <zittlowl@ripon.edu>
2024-11-03 13:00:02 -0600

    Prepping for NSG
    Modified expmt files to have Lucas's suggestions and to be ready to be sent to NSG

--- expermt/2_side_branch_distances.py
+++ expermt/2_side_branch_distances.py
@@ -3,19 +3,19 @@ from gnatsolv.cells.dcell import DCell
 from gnatsolv.tools.environment import DeathEnviro
 from gnatsolv.tools.apdeath import DeathRec
 from neuron import h
-from tqdm import tqdm
 import time
-import progressbar
 h.load_file("stdrun.hoc")
 
-# bar = progressbar.ProgressBar()
-def collect_gna(cell, e, est, err):
+def collect_gna(e, est, err):
+    cell = e.m
     mtx = np.zeros((cell.main_shaft.nseg,cell.main_shaft.nseg))
+    seg_mtx = []
     start = time.perf_counter()
-    for x in tqdm(cell.iter_dist(1)):
+    for l,x in enumerate(cell.iter_dist(1)):
         gna_lst = []
+        seg_mtx.append(x)
         # print(f"NEW SPOT: b1 seg = {x}")
-        for y in tqdm(cell.iter_dist(2)):
+        for y in cell.iter_dist(2):
             # print(f"NEW SPOT: seg = {y}")
             # binary search for g_Na,Thresh given current geometry, up to 9 digits of accuracy
             gna = e.fullsolve(est, err, 1e-9)
@@ -25,15 +25,15 @@ def collect_gna(cell, e, est, err):
             # update the estimate to ensure faster convergence of subsequent binary search
             est = gna
             # print(f"gna for seg {x} = {gna}")
-        mtx[x,:]=gna_lst
+        print(gna_lst)
+        mtx[l,:]=gna_lst P
     end = time.perf_counter()
     print(f"time = {end - start}")
-    return mtx
+    return mtx, seg_mtx
 
-def main(est):
+def main():
 
     cell = DCell()  # set up cell
-    cell.dx = pow(2,-3)
     cell._normalize()
     cell.l[3] = 0  # sets lengths of unneeded branches to 0
     cell.l[1] = cell.l[2] =4.0
@@ -48,17 +48,18 @@ def main(est):
     # set up death recorder and gna solver
     deathrec = DeathRec(cell.main_shaft, cell.main_shaft, 1)
     e = DeathEnviro(cell, deathrec, stim)
-    e.PRINTTIME = True #traacks how long it takes
-    est = est  # estimate for initial gna
+    e.PRINTTIME = True #traacks how long it take
 
     initial_estimate = 0.15
     initial_error = 0.05
 
-    data = collect_gna(
+    gna_data, main_seg_data = collect_gna(
         e,
         initial_estimate, initial_error
     )
-    np.save('2_side_branch_distances_gna_matrix',data)
-
-main(0.15816538)
-# main(0.45)
+    np.save('2_side_branch_distances_gna_matrix1',gna_data)
+    np.save('side_and_sub_branches_distances_seg_matrix_for_main1', main_seg_data)
+    print(f"gna stored in: side_and_sub_branches_distances_gna_matrix1.npy")
+    print(f"seg stored in: side_and_sub_branches_distances_seg_matrix_for_main1.npy")
+if __name__ == '__main__':
+    main()
--- expermt/2_side_branches_lengths.py
+++ expermt/2_side_branches_lengths.py
@@ -11,29 +11,36 @@ h.load_file("stdrun.hoc")
 # bar = progressbar.ProgressBar()
 
 
-def collect_gna(cell, e, est):
-
+def collect_gna(e, est, err):
+    cell = e.m
     mtx = np.zeros((cell.main_shaft.nseg,2))
     start = time.perf_counter()
-    for x in tqdm(cell.iter_length(1)):
+    for l,x in enumerate(cell.iter_length(1)):
         gna_lst = []
         # print(f"NEW SPOT: b1 seg = {x}")
-        for y in tqdm(cell.iter_length(2)):
-            time.sleep(0.1)
-            # h.topology()
+        for y in cell.iter_length(2):
+            h.topology()
             # print(f"NEW SPOT: seg = {y}")
-            gna = e.fullsolve(est, 1e-4, 1e-9)
+            gna = e.fullsolve(est, err, 1e-9)
             gna_lst.append(gna)
+
+            # guess subsequent error based on previous error
+            err = (abs(est - gna) + err) / 2
+            # update the estimate to ensure faster convergence of subsequent binary search
             est = gna
-            # print(f"gna for seg {x} = {gna}")
-            # print(x*4+0.2)
-            # bar.update(i)
+
+        # print(f"gna for seg {x} = {gna}")
+        mtx[l,:] = gna_lst
+    len_mtx = np.linspace(
+                0, cell.lmax[1],
+                int((cell.lmax[1]-0)/(1*cell.dx))
+                )
     end = time.perf_counter()
     print(f"time = {end - start}")
-    return mtx
+    return mtx, len_mtx
 
-def main(est):
-
+def main():
+    global cell
     cell = DCell()  # set up cell
     cell.l[3] = 0  # sets lengths of unneeded branches to 0
     cell.l[1] = cell.l[2] =4.0
@@ -41,22 +48,29 @@ def main(est):
     cell.side[1].connect(cell.main_shaft(0.2)) #
     cell.side[2].connect(cell.main_shaft(0.2))
 
-    # h.topology() #checks the topology of the cell
+    h.topology() #checks the topology of the cell
+    global stim
     stim = h.IClamp(cell.parent(0.5)) #setups the stimulator
     stim.amp = 0.2 #gives it an amp in mV
     stim.delay = 0 #delay of Clamp from start in ms
     stim.dur = 5 / 16 #duration of clamp in ms
 
     # set up death recorder and gna solver
+    global e
     deathrec = DeathRec(cell.main_shaft, cell.main_shaft, 1)
     e = DeathEnviro(cell, deathrec, stim)
-    e.PRINTTIME = True #tracks how long it takes
-    est = est  # estimate for initial gna
+    e.PRINTTIME = True  # enable solver to print runtime of each individual solve
 
-    data = collect_gna(cell,e, est)
-    np.save('2_side_branches_lengths_gna_matrix',data)
-    # h.psection(sec=cell.side[1])
-    # print(cell.side[1].parentseg())
+    initial_estimate = 0.15
+    initial_error = 0.05
 
-main(0.15816538)
-# main(0.45)
+    gna_data, len_data = collect_gna(
+        e,
+        initial_estimate, initial_error
+    )
+    np.save('2_side_branches_lengths_gna_matrix1', gna_data)
+    np.save('side_and_sub_branches_distances_seg_matrix_for_parent1', len_data)
+    # print(f"gna data for 2 branch lengths in 2_side_branches_lengths_gna_matrix1.npy")
+    # print(f"gna data for 2 branch lengths in 2_side_branches_lengths_len_matrix_for_branches1.npy")
+if __name__ == '__main__':
+    main()
