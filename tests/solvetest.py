355cc2e2424dee38639610604da3423ee4292048
lucasg52 <lucasg52@github.com>
2024-11-10 15:19:04 -0600

    added tests for expanding searches

--- tests/solvetest.py
+++ tests/solvetest.py
@@ -24,4 +24,35 @@ class SolveTest(unittest.TestCase):
         e.TARGDT = pow(2,-6)
         calcthresh = e.fullsolve(0.1745, 0.001, 1e-8) 
         self.assertTrue(abs(calcthresh - 0.1745632667541504) < 5e-9)
-
+    def test_expandsolve_down(self):
+        m = DCell()
+        e = DeathEnviro(
+                m = m, 
+                stim = h.IClamp(m.parent(1)),
+                deathrec = DeathRec(
+                    m.main_shaft, m.main_shaft,
+                    tstop = 1
+                    )
+                )
+        e.stim.delay = 0.5
+        e.stim.amp = 0.2
+        e.stim.dur = 5/16
+        e.TARGDT = pow(2,-6)
+        calcthresh = e.fullsolve(0.18, 0.001, 1e-8) 
+        self.assertTrue(abs(calcthresh - 0.1745632667541504) < 5e-9)
+    def test_expandsolve_up(self):
+        m = DCell()
+        e = DeathEnviro(
+                m = m, 
+                stim = h.IClamp(m.parent(1)),
+                deathrec = DeathRec(
+                    m.main_shaft, m.main_shaft,
+                    tstop = 1
+                    )
+                )
+        e.stim.delay = 0.5
+        e.stim.amp = 0.2
+        e.stim.dur = 5/16
+        e.TARGDT = pow(2,-6)
+        calcthresh = e.fullsolve(0.169, 0.001, 1e-8) 
+        self.assertTrue(abs(calcthresh - 0.1745632667541504) < 5e-9)
