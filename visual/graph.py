fff51d15e983dcf7c2cdf7ff725032b57241f0e9
Lucas Swanson <141633910+lucasg52@users.noreply.github.com>
2024-12-06 09:50:52 -0600

    Update graph.py

--- visual/graph.py
+++ visual/graph.py
@@ -1,4 +1,4 @@
-#Lucas Swanson -- Ripon College '27
+#Lucas Swanson & Laura Zittlow -- Ripon College '27
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,24 +8,6 @@ from matplotlib import cm
 #def makeplot(cmap = cm.viridis):
 
 plt.style.use('_mpl-gallery')
-#
-## Make data
-#X = np.arange(-5, 5, 0.25)
-#Y = np.arange(-5, 5, 0.25)
-#X, Y = np.meshgrid(X, Y)
-#R = np.sqrt(X**2 + Y**2)
-#Z = np.sin(R)
-#
-## Plot the surface
-#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-#ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap = cm.viridis)
-#
-#ax.set(xticklabels=[],
-#       yticklabels=[],
-#       zticklabels=[])
-#
-#plt.show()
-
 
 def matrixplot(matx, start1, stop1, start2, stop2):
     rows,cols = matx.shape
@@ -39,10 +21,10 @@ def matrixplot(matx, start1, stop1, start2, stop2):
     ax.plot_surface(X, Y, Z, vmin=Z.min(),  cmap = cm.viridis)
     plt.show()
 
-def matrixplot_smart(matx_x, matx_z): #axisvars):
+def matrixplot_smart(matx_x, matx_z, unitsize = 4): #axisvars):
     rows,cols = matx_z.shape
-    X = matx_x*4 #axisvars[1]
-    Y = matx_x*4#axisvars[0]
+    X = matx_x*unitsize
+    Y = matx_x*unitsize
 
     X, Y = np.meshgrid(X, Y)
     Z = matx_z
