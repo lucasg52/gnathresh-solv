c593dafc5129fe0aa68453d42ca52f3aab53e386
lucasg52 <141633910+lucasg52@users.noreply.github.com>
2024-12-30 17:14:50 -0600

    added test_badestimate

--- tests/envirotest.py
+++ tests/envirotest.py
@@ -1,4 +1,5 @@
 import unittest
+import warnings
 from gnatsolv.cells.base import BaseExpCell
 from gnatsolv.enviro.basic import BasicEnviro 
 from gnatsolv.tools.aprecorder import APRecorder
@@ -13,27 +14,18 @@ class TestCell(BaseExpCell):
         return(f"pycell[{self.gid}]")
     def _create_secs(self):
         self.side = h.Section(name = "side", cell = self)
-        self.main2 = h.Section(name = "main2", cell = self)
     def _setup_bioph(self):
-        for sec in [self.IS, self.main_shaft, self.prop_site, self.side, self.main2]:
+        for sec in [self.IS, self.main_shaft, self.prop_site, self.side]:
             kin.insmod_Traub(sec, "axon")
         kin.insmod_Traub(self.soma, "soma")
     def _connect(self):
         super()._connect()
-        self.main2.connect(self.IS(1))
-        self.main_shaft.connect(self.main2(1))
-        self.prop_site.connect(self.main_shaft(1))
-        self.side.connect(self.main2(1))
+        self.side.connect(self.main_shaft(1/8))
     def _setup_morph(self):
-        # this is a lot of lines
-        # might b more easy to maintain if its just a dcell? maybe not?
         super()._setup_morph()
         self.IS.diam = self.IS_diam
         self.side.L = self.prop_site.L * 3
         self.side.diam = self.prop_site.diam
-        self.main2.diam = self.main_diam
-        self.main2.L = self.main_shaft.L / 8
-        self.main_shaft.L -= self.main_shaft.L / 8
 
 def create_test_enviro():
     cell = TestCell(0.1,3)
@@ -51,5 +43,13 @@ def create_test_enviro():
 class EnviroTest(unittest.TestCase):
     def test_basic(self):
         e = create_test_enviro()
-        e.fullsolve()
+        k = e.fullsolve(0.15, 0.1, 1e-6)
+        self.assertAlmostEqual(k, 0.15032167434692384, places = 6)
+
+    def test_badestimate(self):
+        e = create_test_enviro()
+        with warnings.catch_warnings(record=True) as wlog:
+            k = e.fullsolve(0.1, 0.005, 1e-6)
+            self.assertTrue(wlog)
+        self.assertAlmostEqual(k, 0.15032167434692384, places = 6)
 
