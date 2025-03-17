455edf029d792b3b36376a5ec910632fa331610d
Lucas Swanson <141633910+lucasg52@users.noreply.github.com>
2025-03-17 18:39:23 -0500

    Annotate accexperiment.py

--- expermt/accexperiment.py
+++ expermt/accexperiment.py
@@ -9,8 +9,12 @@ from gnatsolv.tools.environment import DeathEnviro
 #from ..tools.aprecorder import APRecorder
 h.load_file('stdrun.hoc')
 
-def calcthreshes(seed, leng=10):
-    global cell, stim, e, deathrec
+def calcthreshes(
+        seed='gnat',    # Specify an rng seed for morphologies
+        leng=10         # how many cells to create
+    ):
+    global cell, stim, e, deathrec # only for debug.
+    ### MORPHOLOGY PARAMETERS: ###
     cell = DCell()
     cell.dx =pow(2,-7)
     cell._normalize()
@@ -19,6 +23,7 @@ def calcthreshes(seed, leng=10):
     stim.amp = 0.2
     stim.delay = 0
     stim.dur = 5/16
+    cell.update_length(1, 0) # remove one of the side branches
 
     e = DeathEnviro(
             cell,
@@ -27,7 +32,7 @@ def calcthreshes(seed, leng=10):
             )
     ret = []
     random.seed(seed)
-    cell.update_length(1, 0)
+    
     for i in range(leng):
         print(f'calculating threshes for geometry {i+1} out of {leng}')
         cell.l= np.array(
@@ -40,7 +45,8 @@ def calcthreshes(seed, leng=10):
         ret.append(accsweep(e))
     return ret
 
-def normalizerange(est, rad, acc):
+def normalizerange(est, rad, acc): # an attempt to both optimize search radius, and force gNaT into bins of size `acc`, when using an adaptive ExpandingBinSearch sweep
+                                   # To disable, simply call fullsolve (see within accsweep) with raw parameters
     n = int(est/acc)
     est_n = n*acc
     dif = abs(est_n - est)
@@ -50,7 +56,12 @@ def normalizerange(est, rad, acc):
     return est_n, pow(2, i)*acc, acc
 
 
-def accsweep(e, rmax = 40, est_init = 0.15, rad_init = 0.05, acc = pow(2,-14)):
+def accsweep(    # create a list of gNaT computed by environment `e` under bit roundoff from 0 to rmax, with steps of 4 bits
+        e, # environment
+        rmax = 40,    # maximum roundoff. cannot be higher than 51
+        est_init = 0.15, rad_init = 0.05, # initial search estimate and radius
+        acc = pow(2,-14)                  # search accuracy 
+    ):
     est = est_init
     rad = rad_init
     ret = []
@@ -60,13 +71,10 @@ def accsweep(e, rmax = 40, est_init = 0.15, rad_init = 0.05, acc = pow(2,-14)):
         sys.stdout.flush()
         h.roundoff_nafTraub = i
         h.roundoff_kdrTraub = i
-        assert(
-                h.roundoff_nafTraub == i
-                and 
-                h.roundoff_kdrTraub == i
-                )
 
         gnat = e.fullsolve(*normalizerange(est, rad, acc))
+        # disable normalizerange:
+        # gnat = e.fullsolve(est, rad, acc)
         err = abs(est-gnat)
         est = gnat
         rad = (err+acc)*1.5
